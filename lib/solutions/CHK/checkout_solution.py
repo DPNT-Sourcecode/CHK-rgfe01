3381f7582aad8279eb7ea8bccabb9e73b0837c49
sbx_user1051 <sbx_user1051@169.254.17.233>
2024-02-29 15:21:48 +0000

    Thu Feb 29 15:21:48 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@
 def checkout(skus):
     sku_count_map = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0}
     sku_prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
-    special_offers = {'A': [3, 130], 'B': [2, 45]}
+    special_offers = {'A': [[3, 130], [5, 200]], 'B': [[2, 45]]}
 
     for sku in skus:
         if sku not in sku_count_map:
@@ -45,11 +45,12 @@ def checkout(skus):
             print(f'Current count is {sku_count_map[sku]}')
 
             if sku in special_offers:
-                if sku_count_map[sku] % special_offers[sku][0] == 0:
-                    num_of_orders = sku_count_map[sku] / special_offers[sku][0]
-                    total_price += num_of_orders * special_offers[sku][1]
-                    sku_count_map[sku] -= num_of_orders * special_offers[sku][0]
-                    print(f'Just added {num_of_orders * special_offers[sku][1]}')
+                for offer in special_offers[sku]:
+                    if sku_count_map[sku] % offer[0] == 0:
+                        num_of_orders = sku_count_map[sku] / offer[0]
+                        total_price += num_of_orders * offer[1]
+                        sku_count_map[sku] -= num_of_orders * offer[0]
+                        print(f'Just added {num_of_orders * offer[1]}')
 
             if sku_count_map[sku] > 0:
                 sku_count_map[sku] -= 1
@@ -65,10 +66,11 @@ def checkout(skus):
     return total_price
 
 
-result = checkout("AAAAA")
+result = checkout("AAAAAAAA")
 
 
     
+
 
 
 
