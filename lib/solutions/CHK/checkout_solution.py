2dc7b06be740ee79a5ad755326181ce92e8743c7
sbx_user1051 <sbx_user1051@169.254.17.233>
2024-02-29 15:06:40 +0000

    Thu Feb 29 15:06:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,35 +3,63 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    sku_count_map = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
-    sku_prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    sku_count_map = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0}
+    sku_prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
     special_offers = {'A': [3, 130], 'B': [2, 45]}
 
     for sku in skus:
         if sku not in sku_count_map:
+            print(f'{sku} is not in sku maps')
             return -1
         
         sku_count_map[sku] += 1
 
+    print('SKU MAP IS = ', sku_count_map)
+    print('SKU prices is = ', sku_prices)
+
     total_price = 0
     for sku in sku_count_map:
+        # if sku == 'A':
+        #     if sku_count_map[sku] % 3 == 0:
+        #         sets = sku_count_map[sku] / 3
+        #         total_price += sets * 130
+        #         print(f'Just added {sets * 130}')
+
+        #         continue
+
+        # if sku == 'B':
+        #     if sku_count_map[sku] % 2 == 0:
+        #         sets = sku_count_map[sku] / 2
+        #         total_price += sets * 45
+        #         print(f'Just added {sets * 45}')
+        #         continue
 
         while sku_count_map[sku] > 0:
+            print(f'Current count is {sku_count_map[sku]}')
 
             if sku in special_offers:
                 if sku_count_map[sku] % special_offers[sku][0] == 0:
                     num_of_orders = sku_count_map[sku] / special_offers[sku][0]
                     total_price += num_of_orders * special_offers[sku][1]
                     sku_count_map[sku] -= num_of_orders * special_offers[sku][0]
+                    print(f'Just added {num_of_orders * special_offers[sku][1]}')
 
             if sku_count_map[sku] > 0:
                 sku_count_map[sku] -= 1
                 total_price += sku_prices[sku]
+                print(f'Just added {sku_prices[sku]}')
+
+
+        # total_price += sku_count_map[sku] * sku_prices[sku]
+        # print(f'Just added {sku_count_map[sku] * sku_prices[sku]}')
+
+    print(f'Total Price is = {total_price}')
 
     return total_price
 
 
-
+result = checkout("AAAAAAA")
 
 
     
+
